caccca8e321b03b682cfd67ed86dc750fc2371b5
Sara van den Berg <saravandenberg95@gmail.com>
2021-09-21 16:18:03 +0200

    Sort props alphabetically

--- styles/global.ts
+++ styles/global.ts
@@ -2,10 +2,10 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   * {
-    margin: 0;
-    padding: 0;
     box-sizing: border-box;
     list-style: none;
+    margin: 0;
+    padding: 0;
     text-align: center;
   }
 
