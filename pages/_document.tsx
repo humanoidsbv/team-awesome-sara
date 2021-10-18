eb6b12a2a683deb30e49beaf32028f68aecbd8a5
Sara van den Berg <saravandenberg95@gmail.com>
2021-10-18 10:44:41 +0200

    Change types in _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,16 @@
 import React from "react";
-import Document from "next/document";
+import Document, { DocumentContext, DocumentInitialProps } from "next/document";
 import { ServerStyleSheet } from "styled-components";
-import { AppProps } from "next/dist/shared/lib/router/router";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: any) {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App: any) => (props: AppProps) => sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
